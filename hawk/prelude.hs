0846e44e2d27b794641343d4e5288d448fa4320e
Sri Kadimisetty <kadimisetty@users.noreply.github.com>
2020-10-11 12:36:32 +0530

    add hawk config

---
+++ hawk/prelude.hs
@@ -0,0 +1,10 @@
+-- Configuration (Prelude) for hawk
+-- https://github.com/gelisam/hawk/tree/master/doc#user-prelude
+-- hawk expects to see this content (as a ln or otherwise) at:
+--   ~/.hawk/prelude.hs
+
+{-# LANGUAGE ExtendedDefaultRules, OverloadedStrings #-}
+
+import           Prelude
+import qualified Data.ByteString.Lazy.Char8    as B
+import qualified Data.List                     as L
